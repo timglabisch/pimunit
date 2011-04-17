e3cf0a57a52b684927824551943fe9d69d24c385
timg <none@none>
2011-04-17 20:19:08 +0200

    add a abstract Test Class

---
+++ lib/Pimcore/Test/Case/Abstract.php
@@ -0,0 +1,9 @@
+<?
+class Pimcore_Test_Case_Abstract extends PHPUnit_Framework_TestCase {
+
+    public function getFixture($path)
+    {
+        return PIMUNIT_ROOT.'/fixtures/'.$path;
+    }
+
+}
