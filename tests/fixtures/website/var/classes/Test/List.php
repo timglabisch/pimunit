bead20c819371680cb5bb12a828b93e87c2def98
timg <none@none>
2011-06-27 21:50:24 +0200

    load mock custom objects

---
+++ tests/fixtures/website/var/classes/Test/List.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Object_Test_List extends Object_List_Concrete {
+
+public $classId = 1;
+public $className = "test";
+
+
+}
--- tests/tests/models/Object/Class/CustomClassesTest.php
+++ tests/tests/models/Object/Class/CustomClassesTest.php
@@ -10,6 +10,9 @@ class Models_Object_Class_CustomClassesTest extends Pimcore_Test_Case {
         copy($this->getFixture('website/var/classes/definition_1.psf'),PIMCORE_CLASS_DIRECTORY.'/definition_1.psf');
         $this->getDb()->exec(file_get_contents($this->getFixture('website/var/classes/testclass.sql')));
 
+        require_once $this->getFixture('website/var/classes/Test.php');
+        require_once $this->getFixture('website/var/classes/Test/List.php');
+
         $o = new Object_Test();
         $o->setKey('test');
         $o->setParentId(1);
