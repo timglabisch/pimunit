b7af1e5ec1c63052abb81b42267477aa9b84a1e7
timg <tim@ainfach.de>
2011-08-23 16:07:05 -0700

    tidy up
    --HG--
    branch : DiC

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,11 @@ use de\any\di\binder\parser\xml;
 require_once __DIR__.'/lib/Any/di.php';
 require_once __DIR__.'/lib/Any/DI/binder/parser/xml.php';
 
+require_once __DIR__.'/lib/Pimunit/iStartup.php';
+require_once __DIR__.'/lib/Pimunit/Startup/Standard.php';
+require_once __DIR__.'/lib/Pimunit/Startup/iConstants.php';
+require_once __DIR__.'/lib/Pimunit/Startup/Constants/Standard.php';
+
 $xml = new xml(file_get_contents(__DIR__.'/dependencies.xml'));
 
 $repository = new \de\any\di\binder\repository();
@@ -20,59 +25,69 @@ class Pimunit_Bootstrap implements di\iRunable {
     /** @var \de\any\iDi !inject */
     public $di;
 
+    /** @var Pimunit_Startup_iConstants !inject */
+    public $config;
+
     public function run() {
-        // update include paths
-        $includePaths = array(
-            __DIR__.'/lib',
-            __DIR__.'/../../website/var/classes'
-        );
+        $this->config->setPimunitRoot(__DIR__);
 
-        set_include_path(get_include_path().PATH_SEPARATOR.implode(PATH_SEPARATOR, $includePaths));
+        $this->configurePhp();
+        $this->includeStartupFiles();
+        $this->definePimcoreConstants();
+        $this->initShutdownFunction();
+        $this->initPimcore();
+        $this->changeDatabaseDriver();
+        $this->initExternalLibs();
+    }
 
-        require_once __DIR__.'/lib/Pimunit/iStartup.php';
-        require_once __DIR__.'/lib/Pimunit/Startup/Standard.php';
-        require_once __DIR__.'/lib/Pimunit/Startup/iConstants.php';
-        require_once __DIR__.'/lib/Pimunit/Startup/Constants/Standard.php';
+    protected function initExternalLibs() {
+        require_once $this->config->getPimunitRoot().'/lib/Yaml/sfYamlParser.php';
+    }
 
-        define('PIMUNIT_ROOT', __DIR__);
-
-        // php configuration
-        @ini_set("display_errors", "On");
-        @ini_set("display_startup_errors", "On");
-
-        // define Pimunit Root
-        $this->di->get('Pimunit_Startup_iConstants')->setPimunitRoot(__DIR__);
-
-        $startupConstants = $this->di->get('Pimunit_Startup_iConstants');
-
-        // define own contstants
-        define('PIMUNIT_ROOT_PROC', $startupConstants->getPimunitProc());
-        define('PIMUNIT_WEBSITE_PATH', $startupConstants->getPimunitWebsiteDirectory());
-        define('PIMCORE_CONFIGURATION_SYSTEM', $startupConstants->getConfigurationSystemFile());
-        define("PIMCORE_ASSET_DIRECTORY", $startupConstants->getAssetDirectory());
-        define("PIMCORE_VERSION_DIRECTORY", $startupConstants->getVersionDirectory());
-        define("PIMCORE_WEBDAV_TEMP", $startupConstants->getWebdavTempDirectory());
-        define("PIMCORE_LOG_DEBUG", $startupConstants->getLogDebugDirectory());
-        define("PIMCORE_LOG_MAIL_TEMP", $startupConstants->getLogMailTempDirectory());
-        define("PIMCORE_TEMPORARY_DIRECTORY", $startupConstants->getTempDirectory());
-        define("PIMCORE_CACHE_DIRECTORY", $startupConstants->getCacheDirectory());
-        define("PIMCORE_CLASS_DIRECTORY", $startupConstants->getClassDirectory());
-        define("PIMCORE_BACKUP_DIRECTORY", $startupConstants->getBackupDirectory());
-        define("PIMCORE_RECYCLEBIN_DIRECTORY", $startupConstants->getRecyclebinDirectory());
-        define("PIMCORE_SYSTEM_TEMP_DIRECTORY", $startupConstants->getSystemTempDirectory());
-
-        // pimcore constants
-        define('PIMCORE_ADMIN', true);
-
+    protected function initShutdownFunction() {
         register_shutdown_function(function () {
             Pimcore_Resource_Mysql::get()->deleteMockDb();
             $cleanup = new Pimcore_Test_Cleanup();
             $cleanup->rrmdir(PIMUNIT_ROOT_PROC);
             die();
         });
+    }
 
-        // load pimcore
-        require_once PIMUNIT_ROOT . '/../../pimcore/config/startup.php';
+    protected function includeStartupFiles() {
+        $includePaths = array(
+            $this->config->getPimunitRoot().'/lib',
+            $this->config->getPimunitRoot().'/../../website/var/classes'
+        );
+
+        set_include_path(get_include_path().PATH_SEPARATOR.implode(PATH_SEPARATOR, $includePaths));
+    }
+
+    protected function configurePhp() {
+        @ini_set("display_errors", "On");
+        @ini_set("display_startup_errors", "On");
+    }
+
+    protected function definePimcoreConstants() {
+        define('PIMUNIT_ROOT', $this->config->getPimunitRoot());
+        define('PIMUNIT_ROOT_PROC', $this->config->getPimunitProc());
+        define('PIMUNIT_WEBSITE_PATH', $this->config->getPimunitWebsiteDirectory());
+        define('PIMCORE_CONFIGURATION_SYSTEM', $this->config->getConfigurationSystemFile());
+        define("PIMCORE_ASSET_DIRECTORY", $this->config->getAssetDirectory());
+        define("PIMCORE_VERSION_DIRECTORY", $this->config->getVersionDirectory());
+        define("PIMCORE_WEBDAV_TEMP", $this->config->getWebdavTempDirectory());
+        define("PIMCORE_LOG_DEBUG", $this->config->getLogDebugDirectory());
+        define("PIMCORE_LOG_MAIL_TEMP", $this->config->getLogMailTempDirectory());
+        define("PIMCORE_TEMPORARY_DIRECTORY", $this->config->getTempDirectory());
+        define("PIMCORE_CACHE_DIRECTORY", $this->config->getCacheDirectory());
+        define("PIMCORE_CLASS_DIRECTORY", $this->config->getClassDirectory());
+        define("PIMCORE_BACKUP_DIRECTORY", $this->config->getBackupDirectory());
+        define("PIMCORE_RECYCLEBIN_DIRECTORY", $this->config->getRecyclebinDirectory());
+        define("PIMCORE_SYSTEM_TEMP_DIRECTORY", $this->config->getSystemTempDirectory());
+        define('PIMCORE_ADMIN', true);
+    }
+
+    protected function initPimcore() {
+        require_once $this->config->getPimunitRoot() . '/../../pimcore/config/startup.php';
         $pimcore = new Pimcore( );
         $pimcore->initConfiguration();
         $pimcore->setSystemRequirements();
@@ -80,20 +95,16 @@ class Pimunit_Bootstrap implements di\iRunable {
         $pimcore->initLogger();
         $pimcore->initModules();
         $pimcore->initPlugins();
+    }
 
-        // Change the Database Driver
+    protected function changeDatabaseDriver() {
         $dbClass = Zend_Registry::get("pimcore_config_system")->database;
         $reflector = new ReflectionProperty(get_class($dbClass), '_allowModifications');
         $reflector->setAccessible(true);
         $reflector->setValue($dbClass, true);
         Zend_Registry::get("pimcore_config_system")->database->adapter = 'Pimunit';
-
     }
-
 }
 
 $di->run(new Pimunit_Bootstrap());
-
-// load external libs
-require_once PIMUNIT_ROOT.'/lib/Yaml/sfYamlParser.php';
 
