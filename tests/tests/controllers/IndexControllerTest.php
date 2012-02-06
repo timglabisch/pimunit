a7b260da3694e34d6893d16732cb3795bb9d4028
t.glabisch <none@none>
2012-02-06 14:14:41 -0800

    improved api for controller tests

--- tests/tests/controllers/IndexControllerTest.php
+++ tests/tests/controllers/IndexControllerTest.php
@@ -9,7 +9,11 @@ class Pimunit_IndexControllerTest extends Pimcore_Test_Case_Controller {
     }
 
     function testIndex2Action() {
-        $resp = $this->dispatch('/plugin/Pimunit/index/index');
+
+        $request = new Zend_Controller_Request_HttpTestCase();
+        $request->setRequestUri('/plugin/Pimunit/index/index');
+
+        $resp = $this->dispatch($request);
         $this->assertEquals($resp->getResponse()->getBody(), 'Hallo Welt!');
         $this->assertEquals($resp->getController()->view->foo, 'pimunit!');
     }
