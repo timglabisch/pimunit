bba9b5604d5b71ab8f3aa7ae94fd9d635929fd0b
t.glabisch <none@none>
2012-02-18 02:27:37 +0100

    refactoring

--- lib/Pimunit/Db/Sqlbuilder/Standard.php
+++ lib/Pimunit/Db/Sqlbuilder/Standard.php
@@ -37,6 +37,93 @@ class Pimunit_Db_Sqlbuilder_Standard implements Pimunit_Db_iSqlbuilder  {
         return $sql;
     }
 
+    public function addDefaultTableStructure($db) {
+        // insert data into database
+        $db->insert("assets", array(
+            "id" => 1,
+            "parentId" => 0,
+            "type" => "folder",
+            "filename" => "",
+            "path" => "/",
+            "creationDate" => time(),
+            "modificationDate" => time(),
+            "userOwner" => 1,
+            "userModification" => 1
+        ));
+
+        $db->insert("documents", array(
+            "id" => 1,
+            "parentId" => 0,
+            "type" => "page",
+            "key" => "",
+            "path" => "/",
+            "index" => 999999,
+            "published" => 1,
+            "creationDate" => time(),
+            "modificationDate" => time(),
+            "userOwner" => 1,
+            "userModification" => 1
+        ));
+
+        $db->insert("documents_page", array(
+            "id" => 1,
+            "controller" => "",
+            "action" => "",
+            "template" => "",
+            "title" => "",
+            "description" => "",
+            "keywords" => ""
+        ));
+
+        $db->insert("objects", array(
+            "o_id" => 1,
+            "o_parentId" => 0,
+            "o_type" => "folder",
+            "o_key" => "",
+            "o_path" => "/",
+            "o_index" => 999999,
+            "o_published" => 1,
+            "o_creationDate" => time(),
+            "o_modificationDate" => time(),
+            "o_userOwner" => 1,
+            "o_userModification" => 1
+        ));
+
+        $db->insert("users", array(
+            "parentId" => 0,
+            "name" => "system",
+            "admin" => 1,
+            "active" => 1
+        ));
+
+        $db->update("users",array("id" => 0), $db->quoteInto("name = ?", "system"));
+
+
+        $userPermissions = array(
+            array("key" => "assets"),
+            array("key" => "classes"),
+            array("key" => "clear_cache"),
+            array("key" => "clear_temp_files"),
+            array("key" => "document_types"),
+            array("key" => "documents"),
+            array("key" => "objects"),
+            array("key" => "plugins"),
+            array("key" => "predefined_properties"),
+            array("key" => "routes"),
+            array("key" => "seemode"),
+            array("key" => "system_settings"),
+            array("key" => "thumbnails"),
+            array("key" => "translations"),
+            array("key" => "redirects"),
+            array("key" => "glossary" ),
+            array("key" => "reports")
+        );
+
+        foreach ($userPermissions as $up) {
+            $db->insert("users_permission_definitions", $up);
+        }
+    }
+
     public function removeComments($sql) {
         return preg_replace("/\s*(?!<\")\/\*[^\*]+\*\/(?!\")\s*/","", $sql);
     }
--- lib/Pimunit/Db/iSqlbuilder.php
+++ lib/Pimunit/Db/iSqlbuilder.php
@@ -3,4 +3,5 @@ interface Pimunit_Db_iSqlbuilder {
     public function installPimcoreSql();
     public function restoreClassesSql($origConfig, $newDb, $tables);
     public function removeComments($sql);
+    public function addDefaultTableStructure($db);
 }
