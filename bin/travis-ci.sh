1a13355c44bb98e19e123cfd21ad2f6184b7b802
Tim Glabisch <tim@ainfach.de>
2012-07-05 21:12:48 +0300

    Update master

--- bin/travis-ci.sh
+++ bin/travis-ci.sh
@@ -1,14 +1,13 @@
 # install pimcore and handel current directory as Plugin Directory 
-mkdir ~/pimunit
-cd test
-mv ./* ~/pimunit
+mkdir ~/.pimunit
+mv ./* ~/.pimunit
 git clone https://github.com/pimcore/pimcore pimcore
 mv -f pimcore/* .
 mv plugins_example plugins
 mkdir plugins/Pimsolr
 mv website_example website
-mv ~/pimunit/* plugins/Pimsolr/
-chmod -R 777 /website/var
+mv ~/.pimunit/* plugins/Pimsolr/
+chmod -R 777 website/var
 
 # install Pimunit
 mkdir plugins/Pimunit
