1dfd7ec7e177509eae5a5bfb7b336cd2bbb606fe
Tim Glabisch <tim@ainfach.de>
2012-07-12 16:44:49 +0300

    Update master

--- bin/travis-ci.sh
+++ bin/travis-ci.sh
@@ -27,9 +27,7 @@ cat `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` | sed -e "s/
 
 echo "# disable magic quotes"
 cat `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` | sed -e "s/magic_quotes_gpc = On/magic_quotes_gpc = Off/ig" > `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-
-
-echo cat `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` | grep magic_quotes_gpc
+echo "magic_quotes_gpc = Off" > `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 echo "# db"
 mysql -e 'create database pimcore;'
